68a1a8edc80ace5fccaeeb06796364c5f5414721
Myron <hmb87381041@163.com>
2018-05-23 15:29:12 +0800

    create the Profile Color

---
+++ Days/Days/Profile/Color.swift
@@ -0,0 +1,64 @@
+//
+//  Color.swift
+//  Days
+//
+//  Created by Myron on 2018/5/23.
+//  Copyright © 2018年 Myron. All rights reserved.
+//
+
+import UIKit
+
+/** Color Profile */
+class Color {
+    
+    // MARK: - Gray
+    
+    static let white: UIColor = UIColor.white
+    class gray {
+        static let thin: UIColor = UIColor(250,250,250,1)
+        static let light: UIColor = UIColor(240,240,240,1)
+        static let halftone: UIColor = UIColor(160,160,160,1)
+        static let dark: UIColor = UIColor(100,100,100,1)
+    }
+    static let dark: UIColor = UIColor(30,30,30,1)
+    
+    // MARK: - Red
+    
+    class red {
+        static let thin: UIColor = UIColor(255,130,130,1)
+        static let light: UIColor = UIColor(255,60,60,1)
+        static let dark: UIColor = UIColor(255,0,0,1)
+    }
+    
+    // MARK: - Orange
+    
+    class orange {
+        static let thin: UIColor = UIColor(255,160,100,1)
+        static let light: UIColor = UIColor(255,120,34,1)
+        static let dark: UIColor = UIColor(255,100,0,1)
+    }
+    
+    // MARK: - Blue
+    
+    class blue {
+        static let thin: UIColor = UIColor(131,188,255,1)
+        static let light: UIColor = UIColor(40,139,255,1)
+        static let dark: UIColor = UIColor(0,118,255,1)
+    }
+    
+    // MARK: - Green
+    
+    class green {
+        static let thin: UIColor = UIColor(140,255,229,1)
+        static let light: UIColor = UIColor(60,255,211,1)
+        static let dark: UIColor = UIColor(0,255,198,1)
+    }
+    
+    // MARK: - Yellow
+    
+    class yellow {
+        static let thin: UIColor = UIColor(212,255,161,1)
+        static let light: UIColor = UIColor(180,255,93,1)
+        static let dark: UIColor = UIColor(137,255,0,1)
+    }
+}
