2a7438c992bb9d935d275dc32de475291677b828
Myron <hmb87381041@163.com>
2018-05-24 18:12:56 +0800

    create the Key

--- Days/Days/Controller/Habit/Edit/HabitEditTopCard.swift
+++ Days/Days/Controller/Habit/Edit/HabitEditTopCard.swift
@@ -10,13 +10,22 @@ import UIKit
 
 class HabitEditTopCard: CardTopView {
     
+    var habit: Habit {
+        return (table.controller as! HabitEditController).habit
+    }
+    
     override func view_deploy() {
         super.view_deploy()
-        title.text = "新增习惯"
+        title.text = (habit.obj.id == 0 ? "新增习惯" : habit.obj.name)
     }
     
     override func left_action(_ sender: UIButton) {
         table.controller?.dismiss(animated: true, completion: nil)
     }
-
+    
+    override func right_action(_ sender: UIButton) {
+        table.vc?.toSuperController(object: [Key.Habit.append: habit])
+        table.controller?.dismiss(animated: true, completion: nil)
+    }
+    
 }
---
+++ Days/Days/Profile/Key.swift
@@ -0,0 +1,61 @@
+//
+//  Key.swift
+//  Days
+//
+//  Created by Myron on 2018/5/24.
+//  Copyright © 2018年 Myron. All rights reserved.
+//
+
+import Foundation
+
+class Key {
+    
+    class Habit {
+        static let append = "key_habit_append"
+        static let update = "key_habit_update"
+        static let delete = "key_habit_delete"
+        
+        class Unit {
+            static let append = "key_habit_unit_append"
+            static let update = "key_habit_unit_update"
+            static let delete = "key_habit_unit_delete"
+        }
+    }
+    
+    class Event {
+        static let append = "key_event_append"
+        static let update = "key_event_update"
+        static let delete = "key_event_delete"
+        
+        class Unit {
+            static let append = "key_event_unit_append"
+            static let update = "key_event_unit_update"
+            static let delete = "key_event_unit_delete"
+        }
+    }
+    
+    class Chart {
+        static let append = "key_chart_append"
+        static let update = "key_chart_update"
+        static let delete = "key_chart_delete"
+        
+        class Unit {
+            static let append = "key_chart_unit_append"
+            static let update = "key_chart_unit_update"
+            static let delete = "key_chart_unit_delete"
+        }
+    }
+    
+    class Diary {
+        static let append = "key_diary_append"
+        static let update = "key_diary_update"
+        static let delete = "key_diary_delete"
+        
+        class Unit {
+            static let append = "key_diary_unit_append"
+            static let update = "key_diary_unit_update"
+            static let delete = "key_diary_unit_delete"
+        }
+    }
+    
+}
