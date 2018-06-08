15003c28b0cde1ea73ef2a055e5f457735224f37
Myron <hmb87381041@163.com>
2018-06-08 11:17:53 +0800

    完成界面方向控制

--- Days/Days/Profile/BaseController.swift
+++ Days/Days/Profile/BaseController.swift
@@ -10,6 +10,27 @@ import UIKit
 
 class BaseController: ViewController {
     
-    
+    private var _orientations: UIInterfaceOrientationMask?
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if let orient = _orientations {
+            return orient
+        } else {
+            var orient = UIInterfaceOrientationMask.all
+            let device = UIDevice.model()
+            if device.contains("iPhone") {
+                if device.contains("Plus") {
+                    orient = .all
+                } else {
+                    orient = .portrait
+                }
+            } else if device.contains("iPod") {
+                orient = .portrait
+            } else if device.contains("iPad") {
+                orient = .landscape
+            }
+            _orientations = orient
+            return orient
+        }
+    }
     
 }
