7f879fabc480fb6e4a6f425fc5dcb8ac96f17f37
Myron <hmb87381041@163.com>
2018-06-21 09:55:06 +0800

    debug: Sqlite - 后台进入后没有打开数据库。

--- Days/AppDelegate/AppDelegate.swift
+++ Days/AppDelegate/AppDelegate.swift
@@ -45,7 +45,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
-        SQLite.default.open()
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
@@ -57,7 +56,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        SQLite.default.open()
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
